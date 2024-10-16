fcfdc9c2e5bf6ab22a56d5eceb52b04f21169f58
cslearnerer <11579760+cslearnerer@user.noreply.gitee.com>
2024-10-16 21:42:00 +0800

    第一次实验完成

--- L1系统认识实验/program1.asm
+++ L1系统认识实验/program1.asm
@@ -1,20 +1,20 @@
-SSTACK SEGMENT STACK
-    DW 32 DUP(?)
+SSTACK SEGMENT STACK        ;定义堆栈
+           DW 32 DUP(?)
 SSTACK ENDS
 
 CODE SEGMENT
-    ASSUME CS:CODE,SS:SSTACK
-START:
-    PUSH DS
-    XOR AX,AX
-    MOV SI,3000H
-    MOV CX,16
-AA1:
-    MOV [SI],AL
-    INC SI
-    INC AL
-    LOOP AA1
-AA2:
-    JMP AA2
+          ASSUME CS:CODE,SS:SSTACK
+    START:
+          PUSH   DS
+          XOR    AX,AX
+          MOV    SI,3000H             ;数据起始地址
+          MOV    CX,16                ;循环次数，放置数据个数
+    AA1:
+          MOV    [SI],AL
+          INC    SI                   ;地址自加
+          INC    AL                   ;数据自加
+          LOOP   AA1
+    AA2:
+          JMP    AA2
 CODE ENDS
     END START
